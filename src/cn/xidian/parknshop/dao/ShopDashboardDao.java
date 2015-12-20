33dff4b678b13602c4713eb27d34f2ff9072b39e
FeiYue <feiyue9456@foxmail.com>
2015-12-20 13:46:26 +0800

    FeiYue

--- src/cn/xidian/parknshop/dao/ShopDashboardDao.java
+++ src/cn/xidian/parknshop/dao/ShopDashboardDao.java
@@ -2,11 +2,12 @@ package cn.xidian.parknshop.dao;
 
 public interface ShopDashboardDao {
 
-	 long getFavorCount(long shopNo);
-	 
-	 long getNewCommentsCount(long shopNo);
-	 
-	 long getTotalEarnedCount(long shopNo);
-	 
-	 long getNewOrderCount(long shopNo);
+	long getFavorCount(long shopNo);
+
+	long getNewCommentsCount(long shopNo);
+
+	long getTotalEarnedCount(long shopNo);
+
+	long getNewOrderCount(long shopNo);
+
 }
